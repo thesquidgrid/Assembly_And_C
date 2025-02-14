73eff372835d446961a2163703268fdcb2cfbb5a
Sofa Buchman <88793883+thesquidgrid@users.noreply.github.com>
2025-02-14 15:09:42 -0500

    Update lab3_p2.s
    cleaned up code

--- Lab_3_p1/lab3_p2.s
+++ Lab_3_p1/lab3_p2.s
@@ -4,15 +4,18 @@
 .global my_asm_16bitcheck
 
 
+//function to set a bitmask
 my_asm_16bitset:
     ORRS R0, R1             // R0 = R0 | R1 :)
     BX LR                   //return branch back - program counter callback
 
+//function to clear bit based on bitmask
 my_asm_16bitclr:
     MVNS R1, R1             // R1 = NOT(R1)
     ANDS R0, R1             // R0 = R1 AND R0
     BX LR
 
+//function to check if a bit is in a certain position based on a bitmask
 my_asm_16bitcheck:
     ANDS R0, R1             // R0 = R1 & R0
     CMP R0, R1              // compare R0 and R1
@@ -20,7 +23,7 @@ my_asm_16bitcheck:
     MOVS R0, #0             // move value 0 to R0
     BX LR                   //Jump out of function
     
-ret_true:
+ret_true: //tag if R0 and R1 are = to eachother
     MOVS R0, #1             //move value 1 to R0
     BX LR                   //Jump out of function
 
