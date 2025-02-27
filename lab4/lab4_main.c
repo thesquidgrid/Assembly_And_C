59df0c00a62fdf42061c16eaaed0fca151101009
thesquidgrid <skbuchman23@gmail.com>
2025-02-27 15:04:44 -0500

    working build for 7seg display

--- lab4/lab4_main.c
+++ lab4/lab4_main.c
@@ -29,26 +29,37 @@
 #include "LaunchPad.h"
 #include "uart.h"
 
+
 //-----------------------------------------------------------------------------
 // Define function prototypes used by the program
 //-----------------------------------------------------------------------------
 
-
+void delay_ms(uint32_t ms);
 //-----------------------------------------------------------------------------
 // Define symbolic constants used by the program
 //-----------------------------------------------------------------------------
+
+#define LED1_PIN    31
+
+#define LED2_PIN    20
+
+#define LED3_PIN    13
+
+#define LED4_PIN    10
+
+#define LED5_PIN    11
+
+
 const char letters[] =
 {
     0x39,
     0x77,
     0x71,
     0x79
-}
+};
 
 int seg7_digit = 0;
 
-led_disable();
-seg7_enable();
 //-----------------------------------------------------------------------------
 // Define global variables and structures here.
 // NOTE: when possible avoid using global variables
@@ -59,19 +70,34 @@ seg7_enable();
 
 int main(void)
 {
- 
- while(1){
+clock_init_40mhz();
+led_enable();
+led_init();
+seg7_on(2, 0);
+seg7_init();
+launchpad_gpio_init();
 
-    for(seg7_digit =0; seg7_digit < 4; seg7_digit++){
-        seg7_on(letters[seg7_digit], seg7_digit);
-        ms_delay(5);
+
+
+ seg7_init();  // Initialize 7-segment display
+
+    while (1) {
+        for (uint8_t num = 0; num <= 9; num++) {
+            for (uint8_t digit = 0; digit < 4; digit++) {
+                seg7_hex(num, digit);  // Display number on the segment
+                delay_ms(250);         // Delay for visibility
+            }
+        }
     }
 
  }
  
+ void delay_ms(uint32_t ms) {
+    for (volatile uint32_t i = 0; i < (ms * 4000); i++);  // Approximate delay
+}
  // Endless loop to prevent program from ending
  
 
-} /* main */
+ /* main */
 
 
