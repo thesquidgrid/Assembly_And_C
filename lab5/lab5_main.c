ebf19b2c829b1611fdf7995519faafcb45a65b7e
thesquidgrid <skbuchman23@gmail.com>
2025-03-03 14:42:43 -0500

    lab 5 part 1 finished

--- lab5/lab5_main.c
+++ lab5/lab5_main.c
@@ -24,54 +24,72 @@
 // Loads MSP launchpad board support macros and definitions
 //-----------------------------------------------------------------------------
 #include <ti/devices/msp/msp.h>
+
 #include "LaunchPad.h"
+
 #include "clock.h"
 
 
 //-----------------------------------------------------------------------------
 // Define function prototypes used by the program
 //-----------------------------------------------------------------------------
-
-
+void run_lab5_part1();
 //-----------------------------------------------------------------------------
 // Define symbolic constants used by the program
 //-----------------------------------------------------------------------------
-
 
 //-----------------------------------------------------------------------------
 // Define global variables and structures here.
 // NOTE: when possible avoid using global variables
 //-----------------------------------------------------------------------------
 
-
 // Define a structure to hold different data types
 
-int main(void)
-{
-    clock_init_40mhz();
-    launchpad_gpio_init();
-    lp_leds_init();
-    led_init();
-    led_enable();
-    seg7_init();
-    dipsw_init();
+int main(void) {
+   clock_init_40mhz();
+   launchpad_gpio_init();
+   lp_leds_init();
+   led_init();
+   led_enable();
+   led_disable();
+   seg7_init();
+   dipsw_init();
 
+   uint8_t loopCount = 0;
+   bool hexON = false;
+   bool pushButtonOneDown = false;
+   run_lab5_part1();
 
-    uint8_t loopCount = 0;
-    while(loopCount < 5){
-        if(is_pb_down(PB1_IDX)){
-            seg7_hex(0x03);
-            loopCount++;
-        } else{
-            seg7_off();   
-        }
-        while(is_pb_down(PB1_IDX)){
-            msec_delay(10);
-        }
-    }
- 
- // Endless loop to prevent program from ending
- while (1);
+   // Endless loop to prevent program from ending
+   while (1);
 
 } /* main */
 
+
+void run_lab5_part1() {
+
+   uint8_t loopCount = 0;
+   bool hexON = false;
+   bool pushButtonOneDown = false;
+
+   while (loopCount < 3) {
+
+      if (is_pb_down(PB1_IDX)) {
+         if (hexON == false) {
+            seg7_hex(3, 0);
+            msec_delay(10);
+            hexON = true;
+         } else {
+            hexON = false;
+            seg7_off();
+            loopCount++;
+         }
+
+         while (is_pb_down(PB1_IDX)) {
+            msec_delay(15);
+         }
+
+      }
+
+   }
+}
