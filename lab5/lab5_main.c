741662a931705c568b72ca4e408e6529638879b8
thesquidgrid <skbuchman23@gmail.com>
2025-03-07 16:59:07 -0500

    lab 5 done

--- lab5/lab5_main.c
+++ lab5/lab5_main.c
@@ -1,47 +1,44 @@
-//*****************************************************************************
-//*****************************    C Source Code    ***************************
-//*****************************************************************************
-//  DESIGNER NAME:  TBD
-//
-//       LAB NAME:  TBD
-//
-//      FILE NAME:  main.c
-//
-//-----------------------------------------------------------------------------
-//
-// DESCRIPTION:
-//    This program serves as a ... 
-//
-//*****************************************************************************
-//*****************************************************************************
-
-//-----------------------------------------------------------------------------
-// Loads standard C include files
-//-----------------------------------------------------------------------------
+/* **************************************************************************
+ *  LAB NAME:       TBD
+ *  DESIGNER NAME:  TBD
+ *  FILE NAME:      main.c
+ * **************************************************************************
+ *
+ *  DESCRIPTION:
+ *  This program implements four parts of Lab 5. It utilizes GPIO inputs,
+ *  outputs, and a state machine to interact with the MSP microcontroller.
+ *  The lab exercises include:
+ *
+ *  - Part 1: Displaying numbers on a 7-segment display upon button press.
+ *  - Part 2: Capturing DIP switch inputs and displaying the combined values.
+ *  - Part 3: Using a keypad to turn on corresponding LEDs.
+ *  - Part 4: Flashing LEDs based on keypad inputs.
+ *
+ * **************************************************************************/
 
 #include <stdio.h>
-
-//-----------------------------------------------------------------------------
-// Loads MSP launchpad board support macros and definitions
-//-----------------------------------------------------------------------------
 #include <ti/devices/msp/msp.h>
-
 #include "LaunchPad.h"
-
 #include "clock.h"
 
-
-//-----------------------------------------------------------------------------
-// Define function prototypes used by the program
-//-----------------------------------------------------------------------------
+//*****************************************************************************
+// Define function prototypes
+//*****************************************************************************
 void run_lab5_part1();
 void run_lab5_part2();
 void run_lab5_part3();
 void run_lab5_part4();
 void flashAllLEDS(uint8_t);
-//-----------------------------------------------------------------------------
-// Define symbolic constants used by the program
-//-----------------------------------------------------------------------------
+
+//*****************************************************************************
+// Define symbolic constants
+//*****************************************************************************
+#define LOOP_LIMIT  3
+#define LED_FLASHES 500
+#define DELAY_SHORT 1000
+#define DELAY_MED   2000
+#define DELAY_LONG  3000
+#define DEBOUNCE    15
 
 enum States {
     GET_LOW,
@@ -49,161 +46,150 @@ enum States {
     DISPLAY
 } states;
 
-//-----------------------------------------------------------------------------
-// Define global variables and structures here.
-// NOTE: when possible avoid using global variables
-//-----------------------------------------------------------------------------
-
-// Define a structure to hold different data types
-
+//*****************************************************************************
+// Main function
+//*****************************************************************************
 int main(void) {
-   clock_init_40mhz();
-   launchpad_gpio_init();
-   lp_leds_init();
-   lpsw_init();
-   led_init();
-   led_enable();
-   led_disable();
-   seg7_init();
-   dipsw_init();
-   keypad_init();
-   
-   
-   
-   //run_lab5_part1();
-   //run_lab5_part2();
-   //run_lab5_part3();
+    clock_init_40mhz();
+    launchpad_gpio_init();
+    lp_leds_init();
+    lpsw_init();
+    led_init();
+    led_enable();
+    led_disable();
+    seg7_init();
+    dipsw_init();
+    keypad_init();
+
+    run_lab5_part1();
+    msec_delay(LED_FLASHES);
+    run_lab5_part2();
+    msec_delay(LED_FLASHES);
+    run_lab5_part3();
+    msec_delay(LED_FLASHES);
     run_lab5_part4();
-   // Endless loop to prevent program from ending
-   while (1);
 
-} /* main */
-
-
-void run_lab5_part1() {
-
-   uint8_t loopCount = 0;
-   bool hexON = false;
-   bool pushButtonOneDown = false;
-   
-   while (loopCount < 3) {
-
-      if (is_pb_down(PB1_IDX)) {
-         if (hexON == false) {
-            seg7_hex(3, 0);
-            msec_delay(10);
-            hexON = true;
-         } else {
-            hexON = false;
-            seg7_off();
-            loopCount++;
-         }
-
-         while (is_pb_down(PB1_IDX)) {
-            msec_delay(15);
-         }
-
-      }
-
-   }
+    while (1);  // Endless loop
 }
 
+/* **************************************************************************
+ * FUNCTION: run_lab5_part1
+ * DESCRIPTION: Implements a button-controlled 7-segment display.
+ * PARAMETERS: None
+ * RETURNS: None
+ * **************************************************************************/
+void run_lab5_part1() {
+    uint8_t loopCount = 0;
+    bool hexON = false;
+    while (loopCount < LOOP_LIMIT) {
+        if (is_pb_down(PB1_IDX)) {
+            if (!hexON) {
+                seg7_hex(3, 0);
+                msec_delay(DELAY_SHORT / 100);
+                hexON = true;
+            } else {
+                hexON = false;
+                seg7_off();
+                loopCount++;
+            }
+            while (is_pb_down(PB1_IDX)) {
+                msec_delay(DEBOUNCE);
+            }
+        }
+    }
+}
 
+/* **************************************************************************
+ * FUNCTION: run_lab5_part2
+ * DESCRIPTION: Reads DIP switch values and displays them on 7-segment display.
+ * PARAMETERS: None
+ * RETURNS: None
+ * **************************************************************************/
 void run_lab5_part2(){
-    uint8_t lowNib;
-    uint8_t highNib;
-    uint8_t loopCounter = 0;
-    // Initialize state
+    uint8_t lowNib, highNib, loopCounter = 0;
     states = GET_LOW;
-    
-    while (loopCounter < 3) { //suspend state machine after 3 runs
-        // wait until LP_SW2 is pressed
-        while (is_lpsw_up(LP_SW2_IDX) && (states != DISPLAY)); // Wait for button press unless inputs have already been put in         
-        
-        // state machine logic
+    while (loopCounter < LOOP_LIMIT) {
+        while (is_lpsw_up(LP_SW2_IDX) && (states != DISPLAY));
         switch (states) {
             case GET_LOW:
-                msec_delay(3000); //delay for user input
-                lowNib = dipsw_read();  // Read lower nibble
-                states = GET_HIGH;      // Move to next state
+                msec_delay(DELAY_SHORT);
+                lowNib = dipsw_read();
+                states = GET_HIGH;
+                loopCounter++;
                 break;
-
             case GET_HIGH:
-                msec_delay(3000); //delay for user input
-                highNib = dipsw_read();
-                highNib = highNib << 4; // Shift high nibble to upper 4 bits
-                states = DISPLAY;       // Move to next state
+                msec_delay(DELAY_SHORT);
+                highNib = dipsw_read() << 4;
+                states = DISPLAY;
+                loopCounter++;
                 break;
-
             case DISPLAY:
-                msec_delay(2000); //delay for user input
-                if(is_pb_down(PB1_IDX)){
-                    msec_delay(15); // debounce delay
-                    seg7_on(highNib | lowNib, 2); // Display final value at index 2
-                } else{
-                    seg7_on(highNib | lowNib, 0); // Display final value at index 0
-                }
-                states = GET_LOW; // Reset back to first state
-                loopCounter++; //increase loop counter by 1
+                msec_delay(DELAY_MED);
+                seg7_on(highNib | lowNib, is_pb_down(PB1_IDX) ? 2 : 0);
+                states = GET_LOW;
+                loopCounter++;
                 break;
         }
-
-        // Wait until LP_SW1 is released before continuing
         while (is_lpsw_down(LP_SW1_IDX));
-        leds_off(); //cleanup
-        seg7_off(); //cleanup
     }
-    
+    msec_delay(DELAY_LONG);
+    leds_off();
+    seg7_off();
 }
 
+/* **************************************************************************
+ * FUNCTION: run_lab5_part3
+ * DESCRIPTION: Reads keypad input and lights corresponding LED.
+ * PARAMETERS: None
+ * RETURNS: None
+ * **************************************************************************/
 void run_lab5_part3(){
     led_enable();
-    uint8_t loopCounter = 0;
-    uint8_t ledVal = 0;
-    while(loopCounter < 8){
-
+    uint8_t loopCounter = 0, ledVal = 0;
+    while(loopCounter < 9){
         loopCounter++;
-        ledVal = getkey_pressed(); //waits until there is a key pressed - then records the value
-        wait_no_key_pressed(); //prevents the code from speeding past. the user must press and then unpress for the code to continue.
-        leds_on(ledVal); //turn on LED from value gotten from keypad
-        
+        ledVal = getkey_pressed();
+        wait_no_key_pressed();
+        leds_on(ledVal);
     }
-
-    msec_delay(2000);
-    //delay prevents the 8th input from immediately shutting off
-
+    msec_delay(DELAY_MED);
     leds_off();
 }
 
+/* **************************************************************************
+ * FUNCTION: run_lab5_part4
+ * DESCRIPTION: Reads keypad input and flashes LEDs accordingly.
+ * PARAMETERS: None
+ * RETURNS: None
+ * **************************************************************************/
 void run_lab5_part4(){
     led_enable();
-    uint8_t loopCounter = 0;
-    uint8_t ledVal = 0;
+    seg7_off();
+    uint8_t loopCounter = 0, ledVal = 0;
     while(loopCounter < 4){
-
         loopCounter++;
-        while(getkey_pressed() == 0x10); //prevents code from moving forward if there is nothing pressed.
-        ledVal = keypad_scan(); 
-        wait_no_key_pressed(); //prevents the code from speeding past. the user must press and then unpress for the code to continue.
-        flashAllLEDS(ledVal); //turn on LEDs # of times from value gotten from keypad
-        
+        while(getkey_pressed() == 0x10);
+        ledVal = keypad_scan();
+        wait_no_key_pressed();
+        flashAllLEDS(ledVal);
     }
-
-    msec_delay(2000);
-    //delay prevents the 8th input from immediately shutting off
-
+    msec_delay(DELAY_MED);
     leds_off();
 }
 
+/* **************************************************************************
+ * FUNCTION: flashAllLEDS
+ * DESCRIPTION: Flashes all LEDs a specified number of times.
+ * PARAMETERS: numberOfTimes - Number of LED flashes
+ * RETURNS: None
+ * **************************************************************************/
 void flashAllLEDS(uint8_t numberOfTimes){
-
-    uint8_t counter = 0; //loop counter
-    while(counter < numberOfTimes){ //loop goes for as long as the user specifies
-        leds_on(256); //flash all LEDS on
-        msec_delay(500);
+    uint8_t counter = 0;
+    while(counter < numberOfTimes){
+        leds_on(255);
+        msec_delay(LED_FLASHES);
         leds_off();
-        msec_delay(500);
+        msec_delay(LED_FLASHES);
         counter++;
     }
-
 }
