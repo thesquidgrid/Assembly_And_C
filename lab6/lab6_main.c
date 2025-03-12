1ba3d9cc9bddfed021c4b69325ca137f2e7560ea
thesquidgrid <skbuchman23@gmail.com>
2025-03-12 14:39:48 -0400

    with comments lab 6 done!!!!

--- lab6/lab6_main.c
+++ lab6/lab6_main.c
@@ -1,17 +1,21 @@
 //*****************************************************************************
 //*****************************    C Source Code    ***************************
 //*****************************************************************************
-//  DESIGNER NAME:  TBD
+//  DESIGNER NAME:  Sophia Buchman
 //
-//       LAB NAME:  TBD
+//       LAB NAME:  lab6_main.c
 //
 //      FILE NAME:  main.c
 //
 //-----------------------------------------------------------------------------
 //
 // DESCRIPTION:
-//    This program serves as a ... 
-//
+//   This program runs multiple lab exercises on an MSP microcontroller, utilizing
+//   an LCD display, push buttons, and a keypad. It sequentially executes different
+//   functionalities, including displaying characters, handling push-button input,
+//   performing a countdown, and processing keypad input. The program ensures proper
+//   debouncing for buttons and interacts with various peripherals such as a
+//   7-segment display, LEDs, and I2C communication.
 //*****************************************************************************
 //*****************************************************************************
 
@@ -25,8 +29,11 @@
 // Loads MSP launchpad board support macros and definitions
 //-----------------------------------------------------------------------------
 #include <ti/devices/msp/msp.h>
+
 #include "LaunchPad.h"
+
 #include "clock.h"
+
 #include "lcd1602.h"
 
 
@@ -34,10 +41,11 @@
 // Define function prototypes used by the program
 //-----------------------------------------------------------------------------
 void run_lab6_part1();
-void pushButton1();
-void pushButton2();
+void pushButton_release(uint8_t index);
 void run_lab6_part2();
 void run_lab6_part3();
+void run_lab6_part4();
+int8_t padPress();
 //-----------------------------------------------------------------------------
 // Define symbolic constants used by the program
 //-----------------------------------------------------------------------------
@@ -48,182 +56,267 @@ void run_lab6_part3();
 #define displayNumThree (uint8_t) 123
 #define DEBOUNCE 15
 
-//-----------------------------------------------------------------------------
-// Define global variables and structures here.
-// NOTE: when possible avoid using global variables
-//-----------------------------------------------------------------------------
+// start of program
+int main(void) {
+   clock_init_40mhz();
+   launchpad_gpio_init();
+   seg7_init();
+   dipsw_init();
+   I2C_init();
+   lcd1602_init();
+   lcd_clear();
+   keypad_init();
 
+   run_lab6_part1();
+   pushButton_release(PB2_IDX);
+   run_lab6_part2();
+   pushButton_release(PB2_IDX);
+   run_lab6_part3();
+   pushButton_release(PB2_IDX);
+   run_lab6_part4();
 
-// Define a structure to hold different data types
-
-int main(void)
-{   
-    clock_init_40mhz();
-    launchpad_gpio_init();
-    seg7_init();
-    dipsw_init();
-    I2C_init();
-    lcd1602_init();
-    lcd_clear();
-    keypad_init();
-
-    // run_lab6_part1();
-    // pushButton2();
-    // run_lab6_part2();
-    // pushButton2();
-    // run_lab6_part3();
-    // pushButton2();
-
-    //part 4:
-    lcd_clear();
-    lcd_set_ddram_addr(LCD_LINE1_ADDR);
-    lcd_write_string("Running Part 4");
-    msec_delay(1000);
-    lcd_clear();
-    
-    bool flag = false;
-    bool rowFlag = false;
-    int8_t count = 0;
-    int8_t ascii;
-    while(flag == false){
-
-        if(rowFlag == true)){
-            lcd_clear();
-            count = 0;
-            rowFlag = false;
-        }
-
-        if(rowFlag == false){
-            lcd_set_ddram_addr(LCD_LINE1_ADDR + count);
-            count++;
-        } else{
-            lcd_set_ddram_addr(LCD_LINE2_ADDR + count);
-            count++;
-        }
-
-        ascii = getkey_pressed();
-
-        hex_to_lcd(ascii);
-        msec_delay(200);
-        
-        if(is_pb_down(PB1_IDX)){
-            count = 0;
-            lcd_clear();
-            msec_delay(DEBOUNCE);
-        }
-
-        if(is_pb_down(PB2_IDX)){
-            msec_delay(DEBOUNCE);
-            lcd_clear();
-            lcd_write_string("program stopped");
-            flag = true;
-        }
-    }
-
-
-
-    
-    
-
-
-
-
-    
-
-    
-
-
-    
-
- 
- 
- // Endless loop to prevent program from ending
- while (1);
+   // Endless loop to prevent program from ending
+   while (1);
 
 } /* main */
 
-void run_lab6_part1(){
-    for(char letter = 'A'; letter <= 'Z'; letter++){
-        msec_delay(50); //delay for visibility :)
-        lcd_write_char(letter);
-        if(letter=='A'+char_perLine){
-            lcd_set_ddram_addr(LCD_LINE2_ADDR);
-        }
-    }
+// -----------------------------------------------------------------------------
+// DESCRIPTION
+//    This function sequentially displays the letters A to Z on the LCD with a 
+//    delay for visibility. Once the first line reaches its character limit, 
+//    the function moves the cursor to the second line.
+//
+// INPUT PARAMETERS:
+//    none
+//
+// OUTPUT PARAMETERS:
+//    none
+//
+// RETURN:
+//    none
+// -----------------------------------------------------------------------------
+void run_lab6_part1() {
+   for (char letter = 'A'; letter <= 'Z'; letter++) {
+      msec_delay(50); //delay for visibility :)
+      lcd_write_char(letter);
+      if (letter == 'A' + char_perLine) {
+         lcd_set_ddram_addr(LCD_LINE2_ADDR);
+      }
+   }
 }
 
-void pushButton1(){
-    while(is_pb_down(PB1_IDX));
-    msec_delay(DEBOUNCE);
-    while(is_pb_up(PB1_IDX));
-    msec_delay(DEBOUNCE);
+// -----------------------------------------------------------------------------
+// DESCRIPTION
+//    This function handles debouncing for a push button. It waits until the 
+//    button is released to ensure a stable input. The function introduces a 
+//    delay before and after detecting the button release. for debouncing purposes
+//
+// INPUT PARAMETERS:
+//    index - The index of the push button being checked.
+//
+// OUTPUT PARAMETERS:
+//    none
+//
+// RETURN:
+//    none
+// -----------------------------------------------------------------------------
+void pushButton_release(uint8_t index) {
+   while (is_pb_down(index));
+   msec_delay(DEBOUNCE);
+   while (is_pb_up(index));
+   msec_delay(DEBOUNCE);
 }
 
-void pushButton2(){
-    while(is_pb_down(PB2_IDX));
-    msec_delay(DEBOUNCE);
-    while(is_pb_up(PB2_IDX));
-    msec_delay(DEBOUNCE);
+// -----------------------------------------------------------------------------
+// DESCRIPTION
+//    This function disables specific peripherals, displays predefined numeric 
+//    values on the LCD, and waits for a button press between each display update. 
+//    It ends by displaying a message prompting the user to press PB2.
+//
+// INPUT PARAMETERS:
+//    none
+//
+// OUTPUT PARAMETERS:
+//    none
+//
+// RETURN:
+//    none
+// -----------------------------------------------------------------------------
+
+void run_lab6_part2() {
+   //make sure certain peripherals are disabled:
+   led_disable();
+   seg7_disable();
+
+   //start of function
+   lcd_clear();
+   lcd_write_string("Running Part 2");
+   msec_delay(1000);
+   lcd_clear();
+   lcd_set_ddram_addr(LCD_LINE1_ADDR + LCD_CHAR_POSITION_4);
+   lcd_write_quadbyte(displayNumOne);
+   pushButton_release(PB2_IDX);
+   lcd_set_ddram_addr(LCD_LINE2_ADDR);
+   lcd_write_quadbyte(displayNumTwo);
+   pushButton_release(PB2_IDX);
+   lcd_clear();
+   lcd_set_ddram_addr(LCD_LINE1_ADDR + LCD_CHAR_POSITION_7);
+   lcd_write_byte(displayNumThree);
+   pushButton_release(PB2_IDX);
+   lcd_clear();
+   lcd_write_string("Part 2 Done.");
+   lcd_set_ddram_addr(LCD_LINE2_ADDR);
+   lcd_write_string("Press PB2");
+}
+// -----------------------------------------------------------------------------
+// DESCRIPTION
+//    This function performs a countdown from 100 to 0, looping back to 100 
+//    when it reaches zero. The countdown speed is controlled by a delay. 
+//    Pressing PB1 resets the count to 100, and pressing PB2 exits the loop.
+//
+// INPUT PARAMETERS:
+//    none
+//
+// OUTPUT PARAMETERS:
+//    none
+//
+// RETURN:
+//    none
+// -----------------------------------------------------------------------------
+void run_lab6_part3() {
+   lcd_clear();
+   lcd_set_ddram_addr(LCD_LINE1_ADDR);
+   lcd_write_string("Running Part 3");
+   msec_delay(1000);
+   lcd_clear();
+
+   bool flag = false;
+   int8_t count = 100;
+   while (flag == false) {
+
+      lcd_set_ddram_addr(LCD_LINE1_ADDR + LCD_CHAR_POSITION_7);
+      lcd_write_byte(count);
+      count--;
+      msec_delay(200);
+      if (count < 0) {
+         count = 100;
+      }
+
+      if (is_pb_down(PB1_IDX)) {
+         msec_delay(DEBOUNCE);
+         count = 100;
+      }
+
+      if (is_pb_down(PB2_IDX)) {
+         msec_delay(DEBOUNCE);
+         flag = true;
+      }
+   }
+
+   lcd_clear();
+   lcd_write_string("Part 3 Done");
+   lcd_set_ddram_addr(LCD_LINE2_ADDR);
+   lcd_write_string("Press PB2");
 }
 
-void run_lab6_part2(){
-    //make sure certain peripherals are disabled:
-    led_disable();
-    seg7_disable();
-    
-    //start of function
-    lcd_clear();
-    lcd_write_string("Running Part 2");
-    msec_delay(1000);
-    lcd_clear();
-    lcd_set_ddram_addr(LCD_LINE1_ADDR + LCD_CHAR_POSITION_4);
-    lcd_write_quadbyte(displayNumOne);
-    pushButton2();
-    lcd_set_ddram_addr(LCD_LINE2_ADDR);
-    lcd_write_quadbyte(displayNumTwo);
-    pushButton2();
-    lcd_clear();
-    lcd_set_ddram_addr(LCD_LINE1_ADDR + LCD_CHAR_POSITION_7);
-    lcd_write_byte(displayNumThree);
-    pushButton2();
-    lcd_clear();
-    lcd_write_string("Part 2 Done.");
-    lcd_set_ddram_addr(LCD_LINE2_ADDR);
-    lcd_write_string("Press PB2");
+// -----------------------------------------------------------------------------
+// DESCRIPTION
+//    This function handles user input from a keypad and displays characters on 
+//    the LCD. The cursor position alternates between two display lines. Pressing 
+//    PB1 clears the screen and resets the position, while pressing PB2 stops the 
+//    program and displays a termination message.
+//
+// INPUT PARAMETERS:
+//    none
+//
+// OUTPUT PARAMETERS:
+//    none
+//
+// RETURN:
+//    none
+// -----------------------------------------------------------------------------
+void run_lab6_part4() {
+   lcd_clear();
+   lcd_set_ddram_addr(LCD_LINE1_ADDR);
+   lcd_write_string("Running Part 4");
+   msec_delay(1000);
+   lcd_clear();
+
+   bool flag = false;
+   bool rowFlag = false;
+   int8_t count = 0;
+   int8_t ascii = 0;
+   while (flag == false) {
+
+      if (is_pb_down(PB1_IDX)) {
+         count = 0;
+         lcd_clear();
+         msec_delay(DEBOUNCE);
+      }
+
+      if (is_pb_down(PB2_IDX)) {
+         msec_delay(DEBOUNCE);
+         lcd_clear();
+         lcd_write_string("Program stopped");
+         flag = true;
+      }
+
+      if (rowFlag == false) {
+         lcd_set_ddram_addr(LCD_LINE1_ADDR + count);
+         if (count == 16) {
+            count = 0;
+            rowFlag = true;
+         }
+      } else {
+         lcd_set_ddram_addr(LCD_LINE2_ADDR + count);
+         if (count == 16) {
+            count = 0;
+            rowFlag = false;
+         }
+      }
+
+      if ((count == 0 && rowFlag == 0)) {
+         ascii = keypad_scan();
+         if (ascii != 0x10) {
+            lcd_clear();
+            lcd_set_ddram_addr(0x00);
+            padPress();
+            count++;
+         }
+
+      } else {
+         ascii = padPress();
+         if (ascii != 0x10) {
+            count++;
+         }
+
+      }
+
+   }
 }
 
-void run_lab6_part3(){
-    lcd_clear();
-    lcd_set_ddram_addr(LCD_LINE1_ADDR);
-    lcd_write_string("Running Part 3");
-    msec_delay(1000);
-    lcd_clear();
-    
-    bool flag = false;
-    int8_t count = 100;
-    while(flag ==false){
-        
-        lcd_set_ddram_addr(LCD_LINE1_ADDR + LCD_CHAR_POSITION_7);
-        lcd_write_byte(count);
-        count--;
-        msec_delay(200);
-        if(count < 0){
-            count = 100;
-        }
+// -----------------------------------------------------------------------------
+// DESCRIPTION
+//    This function scans for a keypad press, converts the key value to its 
+//    corresponding hexadecimal representation, and displays it on the LCD. 
+//    It ensures no key is pressed before returning the scanned ASCII value.
+//
+// INPUT PARAMETERS:
+//    none
+//
+// OUTPUT PARAMETERS:
+//    none
+//
+// RETURN:
+//    ascii - The ASCII value of the key pressed on the keypad.
+// -----------------------------------------------------------------------------
 
-        if(is_pb_down(PB1_IDX)){
-            msec_delay(DEBOUNCE);
-            count = 100;
-        }
-
-        if(is_pb_down(PB2_IDX)){
-            msec_delay(DEBOUNCE);
-            flag = true;
-        }
-    }
-
-    lcd_clear();
-    lcd_write_string("Part 3 Done");
-    lcd_set_ddram_addr(LCD_LINE2_ADDR);
-    lcd_write_string("Press PB2");
+int8_t padPress() {
+   int8_t ascii = keypad_scan();
+   if (ascii != 0x10) {
+      hex_to_lcd(ascii);
+      msec_delay(200);
+   }
+   wait_no_key_pressed();
+   return ascii;
 }
