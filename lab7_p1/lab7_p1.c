02b6e726b0735e14399760cee0252586ffb207c1
thesquidgrid <skbuchman23@gmail.com>
2025-03-17 15:04:29 -0400

    lab 7 first try

--- lab7_p1/lab7_p1.c
+++ lab7_p1/lab7_p1.c
@@ -24,7 +24,9 @@
 // Loads MSP launchpad board support macros and definitions
 //-----------------------------------------------------------------------------
 #include <ti/devices/msp/msp.h>
+#include "LaunchPad.h"
 #include "clock.h"
+#include "lcd1602.h"
 
 
 //-----------------------------------------------------------------------------
@@ -36,7 +38,7 @@
 // Define symbolic constants used by the program
 //-----------------------------------------------------------------------------
 
-
+#define msg "Hello"
 //-----------------------------------------------------------------------------
 // Define global variables and structures here.
 // NOTE: when possible avoid using global variables
@@ -48,6 +50,21 @@
 int main(void)
 {
  
+    clock_init_40mhz();
+    launchpad_gpio_init();
+    I2C_init();
+    lcd1602_init();
+    bool loopFlag = false;
+    for(int led_add = 0x4F; ((led_add < 0x40) && (loopFlag == false)); led_add++){
+        lcd_set_ddram_addr(led_add);
+        lcd_write_string(msg);
+        msec_delay(500);
+        lcd_clear();
+        if(is_pb_down(PB1_IDX)){
+            loopFlag = true;
+        }
+    }
+    
  
  // Endless loop to prevent program from ending
  while (1);
