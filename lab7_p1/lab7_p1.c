40d1932933235c2ee0c2cfb5bbb4e4c4cceb32e8
thesquidgrid <skbuchman23@gmail.com>
2025-03-21 12:48:18 -0400

    50% lab done

--- lab7_p1/lab7_p1.c
+++ lab7_p1/lab7_p1.c
@@ -26,9 +26,7 @@
 #include <ti/devices/msp/msp.h>
 
 #include "LaunchPad.h"
-
 #include "clock.h"
-
 #include "lcd1602.h"
 
 //-----------------------------------------------------------------------------
@@ -36,7 +34,8 @@
 //-----------------------------------------------------------------------------
 void run_lab7_part1();
 void SysTick_Handler();
-void lcd_string_parser(const char *string, uint8_t start_lcd_addr, uint8_t max_lcd_addr);
+void lcd_string_parser(const char* string, uint8_t start_lcd_addr,
+                       uint8_t max_lcd_addr);
 void run_lab7_part2();
 
 #define MSPM0_CLOCK_FREQUENCY (40e6)
@@ -74,92 +73,111 @@ const uint8_t delay_count[] = {
 // Define a structure to hold different data types
 
 int main(void) {
-  clock_init_40mhz();
-  launchpad_gpio_init();
-  led_init();
-  seg7_init();
-  I2C_init();
-  lcd1602_init();
-  lcd_clear();
-  dipsw_init();
-//   sys_tick_init(SYST_TICK_PERIOD_COUNT);
+   clock_init_40mhz();
+   launchpad_gpio_init();
+   led_init();
+   seg7_init();
+   I2C_init();
+   lcd1602_init();
+   lcd_clear();
+   dipsw_init();
+   sys_tick_init(SYST_TICK_PERIOD_COUNT);
 
-//   SysTick_Handler();
-//   run_lab7_part1();
-//   while(!is_pb_down(PB1_IDX)); //wait for pb1 to be down
+   SysTick_Handler();
 
-//   sys_tick_disable();
-//   seg7_off();
-//   lcd_clear();
+   run_lab7_part1();
+   while (!is_pb_down(PB1_IDX));  // wait for pb1 to be down
 
-//   lcd_set_ddram_addr(0x00);
-//   lcd_write_string("Press PB2");
-//   while(!is_pb_down(PB2_IDX)); //wait for pb2
-  run_lab7_part2();
+   sys_tick_disable();
+   seg7_off();
+   lcd_clear();
 
-  // Endless loop to prevent program from ending
-  while(1);
+   lcd_set_ddram_addr(0x00);
+   lcd_write_string("Press PB2");
+   while (!is_pb_down(PB2_IDX));  // wait for pb2
+   run_lab7_part2();
+   
+   lcd_clear();
+   lcd_set_ddram_addr(0x00);
+   lcd_write_string("Press PB2");
+   while (!is_pb_down(PB2_IDX));  // wait for pb2
+
+   // Endless loop to prevent program from ending
+   while (1);
 
 } /* main */
 
 void run_lab7_part1() {
-  bool done = false;
-  while (!done) {
+   bool done = false;
+   while (!done) {
+      int indx = 0;
 
-    int indx = 0;
+      char msg[] = "Microcontrollers are fun";
 
-    char msg[] = "Microcontrollers are fun";
-
-    for (int led_add = 0x4F; ((led_add > 0x40) && (!done)); led_add--) {
-      lcd_clear();
-      lcd_set_ddram_addr(led_add);
-      lcd_write_string(msg);
-      msec_delay(100);
-      if (is_pb_down(PB1_IDX)) {
-        done = true;
+      for (int led_add = 0x4F; ((led_add > 0x40) && (!done)); led_add--) {
+         lcd_clear();
+         lcd_set_ddram_addr(led_add);
+         lcd_write_string(msg);
+         msec_delay(100);
+         if (is_pb_down(PB1_IDX)) {
+            done = true;
+         }
       }
-    }
-    int indx2 = 1;
-    int address = 0x40;
+      int indx2 = 1;
+      int address = 0x40;
 
-    while ((msg[indx] != '\0') && (!done)) {
-      lcd_clear();
-      lcd_set_ddram_addr(address);
-      lcd_write_string(msg + indx);
-      msec_delay(100);
+      while ((msg[indx] != '\0') && (!done)) {
+         lcd_clear();
+         lcd_set_ddram_addr(address);
+         lcd_write_string(msg + indx);
+         msec_delay(100);
 
-      indx++;
-      if (is_pb_down(PB1_IDX)) {
-        done = true;
+         indx++;
+         if (is_pb_down(PB1_IDX)) {
+            done = true;
+         }
       }
-    }
-  }
+   }
 }
-
 
 void run_lab7_part2() {
-  
-  while (!is_pb_down(PB1_IDX)) {
-    
-    char* msg = "Microcontrollers are fun. I love programming in MSPM0+ assembly code!!!";
-    int led_add = 0x4F;
-    int index = 0;
-    while ((msg[index] != '\0')){
+   char* msg =
+       "Microcontrollers are fun. I love programming in MSPM0+ assembly "
+       "code!!!";
+   int led_add = 0x4F;
+   int index = 0;
+   int index2 = 0;
+   bool done = false;
 
-        lcd_clear();
-        lcd_set_ddram_addr(led_add);
+   while ((msg[index] != '\0') && (!done)) {
+      index++;
 
-        lcd_string_parser(msg, 0x40, 0x50);
-        msec_delay(100);
-        index++;
-        led_add--;
+      if (index > 16) {
+         lcd_clear();
+         lcd_set_ddram_addr(0x40);
+         index2++;
+         lcd_string_parser(msg + index2, 0x40, 0x50);
+         msec_delay(50);
 
-    }
-    
-  }
+         if (is_pb_down(PB2_IDX)) {
+            done = true;
+         }
+
+      } else {
+         lcd_clear();
+         lcd_set_ddram_addr(led_add);
+
+         lcd_string_parser((msg), 0x40, 0x50);
+         msec_delay(100);
+
+         led_add--;
+
+         if (is_pb_down(PB2_IDX)) {
+            done = true;
+         }
+      }
+   }
 }
-
-
 
 //////////////////////////////////////////////////////
 // DESCRIPTION:
@@ -181,31 +199,31 @@ void run_lab7_part2() {
 // none
 ///////////////////////////////////////////////
 void SysTick_Handler(void) {
-  static uint16_t delay_time = 1;
-  static uint16_t code_index = 0;
+   static uint16_t delay_time = 1;
+   static uint16_t code_index = 0;
 
-  delay_time--;
-  if (delay_time == 0) {
-    seg7_on(seg7_letter_code[code_index], SEG7_DIG0_ENABLE_IDX);
+   delay_time--;
+   if (delay_time == 0) {
+      seg7_on(seg7_letter_code[code_index], SEG7_DIG0_ENABLE_IDX);
 
-    delay_time = delay_count[code_index];
-    code_index++;
+      delay_time = delay_count[code_index];
+      code_index++;
 
-    if (code_index == NUM_STATES) {
-      code_index = 0;
-    }
-  }
+      if (code_index == NUM_STATES) {
+         code_index = 0;
+      }
+   }
 }
 
-void lcd_string_parser(const char *string, uint8_t start_lcd_addr, uint8_t max_lcd_addr)
-{
-    uint8_t maxCount = max_lcd_addr - start_lcd_addr;
-    uint8_t count = 0;
-    // for each character in string, write it to the LCD module
-    while ((*string != '\0') && (count < maxCount))
-    {
-        lcd_write_char(*string++);
-        count++;
-    } /* while */
+void lcd_string_parser(const char* string, uint8_t start_lcd_addr,
+                       uint8_t max_lcd_addr) {
+   uint8_t maxCount = max_lcd_addr - start_lcd_addr;
+   uint8_t count = 0;
+   // for each character in string, write it to the LCD module
 
-} /* lcd_write_string */
+   while ((*string != '\0') && (count < maxCount)) {
+      lcd_write_char(*string++);
+      count++;
+   } /* while */
+
+} /* lcd_string_parser */
