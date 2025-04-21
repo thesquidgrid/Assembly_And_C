015c4b9ab7a07b9ae7fdddcd68962a16e5b7112a
thesquidgrid <skbuchman23@gmail.com>
2025-04-21 14:30:52 -0400

    lab10 done.

--- lab10_p1/lab10_p1_main.c
+++ lab10_p1/lab10_p1_main.c
@@ -1,27 +1,29 @@
 //*****************************************************************************
 //*****************************    C Source Code    ***************************
 //*****************************************************************************
-//  DESIGNER NAME:  TBD
+//  DESIGNER NAME:  Sophia
 //
-//       LAB NAME:  TBD
+//       LAB NAME:  Buchman
 //
-//      FILE NAME:  main.c
+//      FILE NAME:  lab10_p1_main.c
 //
 //-----------------------------------------------------------------------------
 //
 // DESCRIPTION:
-//    This program serves as a ... 
-//
+//    This program takes user input via UART and displays it on an LCD1602 screen. 
+//    It supports live character echoing, backspace handling, and ends on Enter key. 
+//    Once complete, it clears the display and shows a "Program Done" message.
 //*****************************************************************************
 //*****************************************************************************
 
 //-----------------------------------------------------------------------------
 // Loads standard C include files
 //-----------------------------------------------------------------------------
+
 #include <stdio.h>
 
-#define enter (char) '\r'
-#define backspace (char) '\b'
+#define enter      '\r'
+#define backspace  '\b'
 
 //-----------------------------------------------------------------------------
 // Loads MSP launchpad board support macros and definitions
@@ -29,49 +31,48 @@
 #include <ti/devices/msp/msp.h>
 
 #include "clock.h"
-
 #include "LaunchPad.h"
-
 #include "adc.h"
-
 #include "lcd1602.h"
-
 #include "uart.h"
 
-int main_uart_lcd_demo(void) {
-   char character;
+int main(void) {
    // Configure the launchpad boards 
    clock_init_40mhz(); 
    launchpad_gpio_init(); 
-   // Configure I2C 
    I2C_init();
-   // Configure LCD 
    lcd1602_init();
-   // Configure the UART 
    UART_init(115200);
-   // Create an endless loop for demonstration purposes 
-   lcd_write_string("NAME:");
-   char currentChar = ' ';
-   uint8_t currentPositon = 0;
-   while (currentChar != '\0') {
-      // wait for character to enter UART character 
-      currentChar = UART_in_char();
-      // echo character back to UART 
-      
-      UART_out_char(character);
-      
-      if(currentChar == enter){
-        currentChar = '\0';
-      } else if(currentChar == backspace){
-        currentPositon--;
-        lcd_set_ddram_addr(currentPositon);
-        currentChar = ' ';
-      }
-      lcd_write_char(currentChar); 
-      currentPositon++;
-   } /* while */
 
+   lcd_write_string("NAME:");
+
+   char currentChar = ' ';
+   uint8_t currentPositon = 5;  // Start after "NAME:"
+   bool nameEnterComplete_flag = false;
+
+   while (!nameEnterComplete_flag) {
+      currentChar = UART_in_char();
+      UART_out_char(currentChar);  // Echo back to terminal
+
+      if (currentChar == enter) {
+         nameEnterComplete_flag = true;
+      } else if (currentChar == backspace) {
+         if (currentPositon > 5) {
+            currentPositon--;
+            lcd_set_ddram_addr(currentPositon + LCD_LINE_NUM_1);
+            lcd_write_char(' '); // Erase character on LCD
+            lcd_set_ddram_addr(currentPositon + LCD_LINE_NUM_1); // Move cursor back
+         }
+      } else {
+         lcd_set_ddram_addr(currentPositon + LCD_LINE_NUM_1);
+         lcd_write_char(currentChar);
+         currentPositon++;
+      }
+   }
+
+   lcd_clear();
    lcd_set_ddram_addr(LCD_LINE_NUM_2);
    lcd_write_string("Program Done");
+
    while(1);
-} /* main */
+}
